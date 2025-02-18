ffc89b5b9282ea561b3d771e4fb5e9a1b18e0517
Kauan Mattos <kauanosmattos@gmail.com>
2025-02-18 11:36:00 -0300

    Primeiro commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
@@ -7,7 +6,51 @@ import { Component } from '@angular/core';
   standalone: false,
 })
 export class HomePage {
+  constructor() {
+  }
+  n1 = 0;
+  n2 = 0;
+  res = 0;
 
-  constructor() {}
+  // Exercicio da Calculadora
+
+  somar() {
+    this.res = this.n1 + this.n2;
+  }
+  subtrair() {
+    this.res = this.n1 - this.n2;
+  }
+  multiplicar() {
+    this.res = this.n1 * this.n2;
+  }
+  dividir() {
+    if (this.n2 != 0) {
+      this.res = this.n1 / this.n2;
+    } else {
+      this.res = 0;
+    }
+  }
+
+// Exercicio 1
+
+    valorDoacao=0;
+    valorDolar=0;
+    valorReais=0;
+
+
+converter() {
+  this.valorReais=this.valorDoacao*this.valorDolar;
+}
+
+
 
 }
+
+
+
+
+
+
+
+
+
